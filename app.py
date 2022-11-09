4b014dcf184f6b0f0f3279b70688aa024d64f261
Kenan Morani <76101633+kenanmorani@users.noreply.github.com>
2022-11-09 13:39:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,9 @@ results =1
 
 for uploaded_file in uploaded_files:
     #if uploaded_file is not None:
-            size = (224,224)  
-            image = ImageOps.fit(uploaded_file, size, Image.ANTIALIAS)
+            size = (224,224) 
+            image_PIL = Image.fromarray(uploaded_file)
+            image = ImageOps.fit(image_PIL, size, Image.ANTIALIAS)
             #image = ImageOps.fit(uploaded_file, (224,224), Image.LANCZOS)
             result = upload_predict(image, UNet_model ,model)
             if result > 0.50:   
